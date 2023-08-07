03f8af31b844005bf30ecf46e6b94a9f204a6775
Dmiriy <kolesov.fsd@gmail.com>
2023-08-07 16:58:08 +0500

    Global component registration

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
+import MyButton from './components/MyButton.vue'
+import MyInput from './components/MyInput.vue'
+import MyModal from './components/MyModal.vue'
+
 const pinia = createPinia();
 
-createApp(App).use(pinia).mount('#app')
+createApp(App)
+    .use(pinia)
+    .component('MyButton', MyButton)
+    .component('MyInput', MyInput)
+    .component('MyModal', MyModal)
+    .mount('#app')
