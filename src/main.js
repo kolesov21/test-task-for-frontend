0b8f5560931bd82b2cf3a9e7fba8b7aa29712a70
Dmiriy <kolesov.fsd@gmail.com>
2023-08-08 00:00:58 +0500

    Auth0 initialize

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue';
 import './style.css'
 import App from './App.vue'
 
@@ -11,6 +12,15 @@ const pinia = createPinia();
 
 createApp(App)
     .use(pinia)
+    .use(
+       createAuth0({
+            domain: "dev-azueegiumdojwgid.eu.auth0.com",
+            clientId: "5LlPAnRjraudw5iJikzZzfbS95riJK0N",
+            authorizationParams: {
+                redirect_uri: 'http://localhost:5173/'
+            }
+        }) 
+    )
     .component('MyButton', MyButton)
     .component('MyInput', MyInput)
     .component('MyModal', MyModal)
